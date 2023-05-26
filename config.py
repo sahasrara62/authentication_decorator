f94dda778ddd57b897ac26f4656bcb8a8eddac1c
Prashant Rana <18409140+sahasrara62@users.noreply.github.com>
2023-05-26 15:46:42 +0400

    Refactoring file to make project more modular and scaleable

--- config.py
+++ config.py
@@ -1,17 +1,12 @@
 import os
 
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
-    # ...
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'api.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, "api.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'd9874b1c9d7d19b255c72a8096ecbd331f6885e9'
+    SECRET_KEY = "d9874b1c9d7d19b255c72a8096ecbd331f6885e9"
 
 
-app_config = {
-
-    'default': Config
-}
+app_config = {"default": Config}
--- library/utils.py
+++ library/utils.py
@@ -1,55 +1,59 @@
-from sqlalchemy.orm import backref
-from flask import request, jsonify, make_response
-from library.main  import app, db
 from functools import wraps
+
 import jwt
+from flask import jsonify, make_response, request
+
+from config import Config
 # from flask_restful import abor
-import uuid
-from werkzeug.security import check_password_hash, generate_password_hash
 from library.models import User
+
 
 # token decorator
 def token_required(f):
-	@wraps(f)
-	def decorator(*args, **kwargs):
-		token=None
-		# pass jwt-token in headers
-		if 'x-access-token' in request.headers:
-			token=request.headers['x-access-token']
-		if not token:  # throw error if no token provided
-			return make_response(jsonify({"message": "A valid token is missing!"}), 401)
-		try:
-			data=jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-			current_user=User.query.filter_by(public_id=data['public_id']).first()
-		except:
-			return make_response(jsonify({"message": "Invalid token!"}), 401)
-		
-		return f(*args, **kwargs)
-	
-	return decorator
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = None
+        # pass jwt-token in headers
+        if "x-access-token" in request.headers:
+            token = request.headers["x-access-token"]
+        if not token:  # throw error if no token provided
+            return make_response(jsonify({"message": "A valid token is missing!"}), 401)
+        try:
+            data = jwt.decode(token, Config.SECRET_KEY, algorithms=["HS256"])
+            current_user = User.query.filter_by(public_id=data["public_id"]).first()
+        except:
+            return make_response(jsonify({"message": "Invalid token!"}), 401)
+        return f(*args, **kwargs)
+    return decorator
 
 
 def permission_required(permission):
-	def deco(f):
-		@wraps(f)
-		def decorator(*args, **kwargs):
-			token=None
-			# pass jwt-token in headers
-			if 'x-access-token' in request.headers:
-				token=request.headers['x-access-token']
-			if not token:  # throw error if no token provided
-				return make_response(jsonify({"message": "A valid token is missing!"}), 401)
-			try:
-				data=jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-				current_user=User.query.filter_by(public_id=data['public_id']).first()
-				current_user_permissions=current_user.permission
-			except:
-				return make_response(jsonify({"message": "Invalid token"}), 401)
-			
-			if not current_user_permissions or not (current_user_permissions in permission):
-				return make_response({"message": "Not authorized", })
-			return f(*args, **kwargs)
-		
-		return decorator
-	
-	return deco
+    def deco(f):
+        @wraps(f)
+        def decorator(*args, **kwargs):
+            token = None
+            # pass jwt-token in headers
+            if "x-access-token" in request.headers:
+                token = request.headers["x-access-token"]
+            if not token:  # throw error if no token provided
+                return make_response(
+                    jsonify({"message": "A valid token is missing!"}), 401
+                )
+            try:
+                data = jwt.decode(token, Config.SECRET_KEY, algorithms=["HS256"])
+                current_user = User.query.filter_by(public_id=data["public_id"]).first()
+                current_user_permissions = current_user.permission
+            except:
+                return make_response(jsonify({"message": "Invalid token"}), 401)
+
+            if not current_user_permissions or not (
+                current_user_permissions in permission
+            ):
+                return make_response(
+                    {
+                        "message": "Not authorized",
+                    }
+                )
+            return f(*args, **kwargs)
+        return decorator
+    return deco
